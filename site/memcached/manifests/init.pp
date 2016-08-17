86a724f51dc20086366d14abb95212eeb34e9825
osdelapaz <osdelapaz@gmail.com>
2016-08-17 11:54:27 +0800

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,6 +1,9 @@
 class memcached {
   file {'/etc/sysconfig/memcached':
   ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
   source => 'puppet:///modules/memcached/memcached',
   require => Package['memcached'],
   }
