88f8aa9654b10120d441756cfb82661b17503671
osdelapaz <osdelapaz@gmail.com>
2016-08-17 09:12:08 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,15 +1,10 @@
 class skeleton {
-  file {'/etc/skel':
+  file { '/etc/skel':
   ensure => directory,
-  owner => 'root',
-  group => 'root',
-  mode => '0755',
   }
-  file {'/etc/skel/.bashrc':
+
+  file { '/etc/skel/.bashrc':
   ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
   source => 'puppet:///modules/skeleton/bashrc',
   }
 }
