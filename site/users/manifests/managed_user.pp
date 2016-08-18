c4419d193cc96247682e47e38124ab61a60133e9
osdelapaz <osdelapaz@gmail.com>
2016-08-18 11:58:42 +0800

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,18 @@
+define users::managed_user {
+) {
+  user { $title:
+  ensure => present,
+  }
+  file { "/home/${title}":
+  ensure => directory,
+  owner => $title,
+  group => $title,
+  mode => '0700',
+  }
+  file { "/home/${title}/.ssh":
+  ensure => directory,
+  owner => $title,
+  group => $title,
+  mode => '0700',
+  }
+}
