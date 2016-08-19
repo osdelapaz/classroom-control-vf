a8313e03b8fbcd067d00709b19053bec1e2c2ba9
osdelapaz <osdelapaz@gmail.com>
2016-08-19 11:36:56 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,62 +1,17 @@
 class nginx (
-$root = undef,
-){
-case $::osfamily {
-
-  'redhat':{
-  $user = 'nginx'
-#  $webroot = '/var/www'
-  $log_dir = '/var/log/nginx'
-  $conf_dir = '/etc/nginx'
-  $owner = 'root'
-  $group = 'root'
-  package { 'nginx':
-    ensure => present,
-    }
-  $default_webroot = '/var/www'
-  }
-
-  'debian':{
-  $user = 'www-data'
-#  $webroot = '/var/www'
-  $log_dir = '/var/log/nginx'
-  $conf_dir = '/etc/nginx'
-  $owner = 'root'
-  $group = 'root'
-  package { 'nginx':
-    ensure => present,
-    }
-  $default_webroot = '/var/www'
-  }
-
-  'windows':{
-  $user = 'nobody'
-#  $webroot = 'C:/ProgramData/nginx/html'
-  $log_dir = 'C:/ProgramData/nginx/logs'
-  $conf_dir = 'C:/ProgramData/nginx'
-  $owner = 'Administrator'
-  $group = 'Administrators'
-  package { 'nginx-service':
-    ensure => present,
-    }
-  $default_webroot = 'C:/ProgramData/nginx/html'
-  }
-  
-  default :{
-    fail("Service is not available in ${::osfamily}")
-  }
-
-}
-
-$webroot = $root ? {
-undef => $default_webroot,
-default => $root,
-} 
+$user = $nginx::params::user,
+$webroot = $nginx::params::webroot,
+$conf_dir = $nginx::params::conf_dir,
+$log_dir = $nginx::params::log_dir,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$package = $nginx::params::package,
+) inherits nginx::params {
 
 File {
-owner => $owner,
-group => $group,
-mode => '0644',
+  owner => $owner,
+  group => $group,
+  mode => '0644',
 }
 
 file {[$webroot, $log_dir, $conf_dir]:
